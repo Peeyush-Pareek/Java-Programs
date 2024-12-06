5de75667a00a357df4a2a5d0420826401f1d4cc5
Peeyush <114051459+Peeyush-Pareek@users.noreply.github.com>
2024-12-06 16:04:52 +0530

    update

---
+++ src/ButtonExample.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+import java.awt.event.*;  
+import javax.swing.*; 
+/**
+ * @author apple
+ *
+ */
+public class ButtonExample {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		  JFrame f=new JFrame("Button Example");  
+		    final JTextField tf=new JTextField();  
+		    tf.setBounds(60,60, 150,20);  
+		    JButton b=new JButton("Click Here");  
+		    b.setBounds(50,100,95,30);  
+		    b.addActionListener(new ActionListener(){  
+		public void actionPerformed(ActionEvent e){  
+		            tf.setText("Welcome to Java GUI Using Swings");  
+		        }  
+		    });  
+		    f.add(b);f.add(tf);  
+		    f.setSize(400,400);  
+		    f.setLayout(null);  
+		    f.setVisible(true);  
+
+	}
+
+}
